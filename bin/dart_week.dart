2c28c499e04af3d4c15307f7f681a369b93bdb3a
thelenaza <kaxtie07@gmailcom>
2022-08-08 11:23:56 +0700

    first commit

---
+++ bin/dart_week.dart
@@ -0,0 +1,8 @@
+void main(){
+  int i = 1;
+
+  while (i <= 5){
+    print('2x$i = ${i*2}');
+    i = i + 1;
+  }
+}
---
+++ bin/dowhile.dart
@@ -0,0 +1,9 @@
+void main(){
+  int i = 1;
+
+  do {
+    print(i);
+    i = i + 1 ;
+
+  } while (1 <= 4);
+}
